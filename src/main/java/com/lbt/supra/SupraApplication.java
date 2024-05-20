32ec1a73a8cafffb27ca86c18eae9d1d8230c732
LBT <fnight644@gmail.com>
2024-05-20 19:07:49 +0700

    initialize project

---
+++ src/main/java/com/lbt/supra/SupraApplication.java
@@ -0,0 +1,13 @@
+package com.lbt.supra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupraApplication.class, args);
+	}
+
+}
